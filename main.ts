e868eb72b9823ecc256d3d82a40917caf24ad355
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-12-08 09:07:48 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
     radio.sendNumber(4)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    radio.sendNumber(6)
+})
+basic.showIcon(IconNames.No)
+radio.setGroup(79)
 basic.forever(function () {
 	
 })
