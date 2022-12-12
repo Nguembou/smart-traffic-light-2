06586e984ef5926cb71437ce379ceaf7db0bc187
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-12-12 09:52:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    radio.sendNumber(4)
+    Entry = ["A"]
+})
+input.onButtonPressed(Button.AB, function () {
+    if (password == Entry) {
+        radio.sendNumber(4)
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.showIcon(IconNames.Yes)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    Entry = [""]
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    radio.sendNumber(6)
+    Entry = ["B"]
 })
+let Entry: string[] = []
+let password: string[] = []
 basic.showIcon(IconNames.No)
+password = [
+"A",
+"B",
+"A",
+"A"
+]
+Entry = [""]
 radio.setGroup(79)
-basic.forever(function () {
-	
-})
+pins.servoWritePin(AnalogPin.P0, 0)
